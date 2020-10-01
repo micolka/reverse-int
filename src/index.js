565c495f79aa6241ebd4e7db8967a5278e700482
micolka <mic_la@tut.by>
2020-10-01 22:47:19 +0300

    feat: resolve reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  if (n > 0) {
+    return String(n).split('').reverse().join('');
+  } else {
+    let arr = String(n).split('').reverse();
+    arr.pop()
+    return arr.join('')
+  }
 }
